43a462f63136cad775a1151054eba1b8cdac87df
Lucas M. Nuñez <114788609+Lucas-M-Nunez@users.noreply.github.com>
2026-03-26 10:59:05 -0300

    update script.js

--- script.js
+++ script.js
@@ -46,7 +46,163 @@
 
 
 //Ejercicio 7:
-let num = 2;
-let tipoDeDato = typeof(num);
-console.log( `el tipo de dato es: ${tipoDeDato}`);
+// let num = 2;
+// let tipoDeDato = typeof(num);
+// console.log( `el tipo de dato es: ${tipoDeDato}`);
 
+
+// ------------------------------------------------------------------------------------------------------------------------
+//Ejercicios:
+
+//ejercicio 1: Crea una variable nombre con tu nombre y úsala para imprimir un mensaje.
+
+// let nombre = "lucas";
+// console.log("Hola " + nombre);
+
+//ejercicio 2: Declara y suma dos números, muestra el resultado en consola.
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1 + num2);
+
+//ejercicio 3: Usa operadores lógicos para evaluar si 10 > 5 y 5 < 2.
+
+// let num3 = 10;
+// let num4 = 5;
+
+// console.log( num3 > num4 && num4 < 2);
+
+//ejercicio 4: Declara variables con let y const y nota las diferencias al intentar cambiarlas.
+
+// let edad = 20;
+// const apellido = 'Gonzalez';
+
+// console.log(edad);
+// console.log(apellido);
+
+// edad = 25;
+// apellido = "Ramirez";
+
+// console.log(edad);
+// console.log(apellido);
+
+//ejercicio 5: Escribe un comentario que explique qué hace tu código.
+
+// Declaramos las variables y asignamos sus valores
+// let primerNum = 10;
+// let segNum = 20;
+// Sumamos las variables
+// let suma = primerNum + segNum;
+// Imprimimos el resultado
+// console.log(suma);
+
+//ejercicio 6: Declara una variable de tipo boolean y úsala en una operación lógica.
+
+//logico
+// let bolean1 = true;
+// let bolean2 = false;
+
+// let res = bolean1 || bolean2
+// console.log(res);
+
+// comparativo
+// let edadPersona = 15;
+
+// if ( edadPersona >= 18 ) {
+//     console.log( "es mayor de edad");
+// } else {
+//     console.log("es menor de edad");
+// }
+
+//ejercicio 7: Imprime el tipo de dato de una variable usando typeof.
+
+// let DNI = 43234534;
+// console.log('Tipo de dato: '+typeof DNI);
+
+//ejercicio 8: Declara un array y muestra uno de sus elementos en la consola.
+
+// let frutas = ['manzana','banana','pera'];
+// console.log(frutas[2]);
+
+
+//ejercicio 9: Usa una operación con % para encontrar el resto de 17 ÷ 3.
+
+// let num1 = 17;
+// let num2 = 3;
+
+// console.log(num1 % num2);
+
+//ejercicio 10: Declara una variable sin valor e imprime su valor inicial.
+
+// let nombre;
+// console.log(nombre); // undefined
+
+
+// --------------------------------------
+
+// ESTRUCTURAS DE COONTROL
+
+//Condicionales: if y else
+
+    // let edad = 10;
+    // if (edad >= 18) {
+    //     console.log("Es mayor de edad");
+    // } else {
+    //     console.log("Es menor de edad");
+    // }
+
+// Switch : multiples casos
+// let dia = 'lunes';
+// switch (dia) {
+//     case 'lunes':
+//         console.log("dia: " + dia);
+//         break;
+//     case 2:
+//         console.log("martes");
+//         break;
+//     case 3:
+//         console.log("miercoles");
+//         break;
+//     case 4:
+//         console.log("jueves");
+//         break;
+//     case 5:
+//         console.log("viernes");
+//         break;
+//     case 6:
+//         console.log("sabado");
+//         break;
+//     case 7:
+//         console.log("domingo");
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// for recorremos arrays con esto.
+
+// const frutas = ['Manzana','pera','banana','uva']
+
+// for (let i = 0; i < frutas.length; i++) {
+//     const element = frutas[i];
+//     console.log("En la posicion "+ i + " , esta el elemento: "+element);
+// }
+
+
+// conteo decreciente de 2 a -10
+// for (let i = 2; i >= -10; i--) {
+//     console.log(i);
+// }
+
+
+//Funciones: 
+
+let nombre = 'lucas';
+
+function saludar(nombre) {
+    return 'hola '+ nombre
+}
+let saludo = saludar(nombre);
+console.log(saludo);
