36f00b0edd9bdf3488a5d371a7a9f108fe9f7ef0
Lucas M. Nuñez <114788609+Lucas-M-Nunez@users.noreply.github.com>
2026-04-06 13:07:28 -0300

    update script tercera clase

---
+++ scripts/tercerclase.js
@@ -0,0 +1,21 @@
+// let precio = 60000;
+// let esPremium = false;
+
+// if (precio > 50000 || esPremium) {
+//     console.log("Este producto tiene envío gratis");
+// } else {
+//     console.log("Debe abonar el costo de envío");
+// }
+
+const btnClick = document.getElementById("btnclick");
+
+function calcular() {
+    let num = parseFloat(prompt("Ingrese un número"));
+    let num2 = parseFloat(prompt("Ingrese otro número"));
+    let resultado = num * num2;
+    alert("El resultado es: "+ resultado);
+}
+
+
+const inputs = document.querySelectorAll(".item");
+console.log(inputs);
