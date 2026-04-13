543f3bdd98342b248ae605097f64aa46f8362d4a
Lucas M. Nuñez <114788609+Lucas-M-Nunez@users.noreply.github.com>
2026-04-12 21:17:22 -0300

    Update tercerclase.js
    tercera clase de JS donde vimos Selectores de JS, document

--- scripts/tercerclase.js
+++ scripts/tercerclase.js
@@ -7,15 +7,15 @@
 //     console.log("Debe abonar el costo de envío");
 // }
 
-const btnClick = document.getElementById("btnclick");
+// const btnClick = document.getElementById("btnclick");
 
-function calcular() {
-    let num = parseFloat(prompt("Ingrese un número"));
-    let num2 = parseFloat(prompt("Ingrese otro número"));
-    let resultado = num * num2;
-    alert("El resultado es: "+ resultado);
-}
+// function calcular() {
+//     let num = parseFloat(prompt("Ingrese un número"));
+//     let num2 = parseFloat(prompt("Ingrese otro número"));
+//     let resultado = num * num2;
+//     alert("El resultado es: "+ resultado);
+// }
 
 
-const inputs = document.querySelectorAll(".item");
-console.log(inputs);
+// const inputs = document.querySelectorAll(".item");
+// console.log(inputs);
